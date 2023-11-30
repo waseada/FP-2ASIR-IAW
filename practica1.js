74396fcd95e1785b9e2c48b7f9a278c08fff2f4b
Javier M <j.manassolano@gmail.com>
2023-11-30 19:27:37 +0100

    a

---
+++ practica1.js
@@ -0,0 +1,10 @@
+import {secundaria} from './practica1.js'
+
+let principal = 100
+
+function saludar (nombre){
+    let resultado = document.getElementById("resultado")
+    let nombre = document.getElementById("campoNombre")
+    resultado.innerHTML = nombre
+}
+
